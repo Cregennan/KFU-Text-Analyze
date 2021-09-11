00cf4fc80fe9dd43b7ebac885747d50abc3eecae
Тахир Латыпов <TALatypov@stud.kpfu.ru>
2021-09-11 13:01:06 +0300

    Главная страница

---
+++ KFU-Text-Analyze/InputText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KFU_Text_Analyze
+{
+    class InputText
+    {
+        private static String Text { get; set; } = "";
+        private static InputText instance = null;
+        private InputText()
+        {
+        }
+        public static InputText GetInstance()
+        {
+            if (InputText.instance == null)
+            {
+                instance = new InputText();
+            }
+            return instance;
+        }
+    }
+}
